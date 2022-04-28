936923348ef321166333e5c0ca7a0f1b7b0d476a
SergeyMont <molotser@mail.ru>
2022-04-28 17:14:46 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/catsgram/exception/UserAlreadyExistException.java
@@ -0,0 +1,5 @@
+package ru.yandex.practicum.catsgram.exception;
+
+public class UserAlreadyExistException extends RuntimeException{
+    public UserAlreadyExistException(String s){super(s);}
+}
---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.catsgram.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Data
+public class User {
+    private String email;
+    private String nickname;
+    private LocalDate birthdate;
+}
